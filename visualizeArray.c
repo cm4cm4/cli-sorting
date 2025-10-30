dbe43f08ae9caa0225d4a383cf9b3bcaa970e96a
cm4cm4 <korawitlongjun452@gmail.com>
2025-10-30 11:45:34 +0700

    fixed off centered start of hash bar

--- visualizeArray.c
+++ visualizeArray.c
@@ -2,9 +2,19 @@
 
 void visualizeArray(long* array, int len)
 {
+	int max = array[0];
+    for (int i = 1; i < len; i++)
+	if (array[i] > max) max = array[i];
+	
+    int width = 0;
+    while (max > 0)
+	{
+		width++;
+		max /= 10;
+	}
     for (int i = 0; i < len; i++)
     {
-        printf("%li ", array[i]);
+   printf("%*li | ",width ,array[i]);
         for (int j = 0; j < array[i]; j++)
         {
             printf("#");
